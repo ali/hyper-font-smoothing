b4e77ef8e32741311127de474d2ca12a0d473ccd
Ali Ukani <ali.ukani@gmail.com>
2016-10-23 01:37:54 -0400

    style(whitespace): clean up whitespace

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ exports.decorateConfig = (config) => {
   return Object.assign({}, config, {
     termCSS: `
       ${config.termCSS || ''}
-      @media 
-      (-webkit-max-device-pixel-ratio: 1.3), 
-      (max-resolution: 120dpi) { 
+      @media
+      (-webkit-max-device-pixel-ratio: 1.3),
+      (max-resolution: 120dpi) {
         x-screen {
           -webkit-font-smoothing: subpixel-antialiased !important;
         }
